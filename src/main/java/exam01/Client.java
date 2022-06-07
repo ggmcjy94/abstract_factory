5db0e07bb33aa660ecc6ff65d079f8799fbd04eb
“ggmcjy94” <lhs01071526524@gmail.com>
2022-06-07 13:30:49 +0900

    추상 팩토리

--- src/main/java/exam01/Client.java
+++ src/main/java/exam01/Client.java
@@ -4,8 +4,11 @@ public class Client {
 
     public static void main(String[] args) {
         MyDialog dialog = new MobileDialog();
+        DesktopDialog desktopDialog = new DesktopDialog();
         Button button = dialog.createButton();
+        Button desktopDialogButton = desktopDialog.createButton();
         button.onClick();
+        desktopDialogButton.onClick();
 
     }
 }
